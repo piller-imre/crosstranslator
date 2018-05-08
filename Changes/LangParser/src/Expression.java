2183bf3e689fba45fcb0ddef0344bba2a10a2422
Manfegor <manfegor@gmail.com>
2018-05-08 23:56:05 +0200

    Add files via upload

--- Changes/LangParser/src/Expression.java
+++ Changes/LangParser/src/Expression.java
@@ -1,3 +1,3 @@
 public class Expression {
-	private String name;
+	
 }
--- Changes/LangParser/src/Variable.java
+++ Changes/LangParser/src/Variable.java
@@ -1,4 +1,23 @@
 public class Variable extends Expression {
 	private String name;
 	private Enum<VariableType> type;
+	public Variable(String name, Enum<VariableType> type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Enum<VariableType> getType() {
+		return type;
+	}
+	public void setType(Enum<VariableType> type) {
+		this.type = type;
+	}
+	
+	
 }
