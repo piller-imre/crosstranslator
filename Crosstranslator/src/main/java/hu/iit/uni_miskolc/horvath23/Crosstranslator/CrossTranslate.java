84d2ef4bc841692ba1b00933816cda66fc61b94b
Manfegor <Manfegor@DESKTOP-PRIK19N>
2018-11-15 02:31:50 +0100

    added action to the buttons, modify listener

--- Crosstranslator/src/main/java/hu/iit/uni_miskolc/horvath23/Crosstranslator/CrossTranslate.java
+++ Crosstranslator/src/main/java/hu/iit/uni_miskolc/horvath23/Crosstranslator/CrossTranslate.java
@@ -1,94 +1,88 @@
 package hu.iit.uni_miskolc.horvath23.Crosstranslator;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
 
-import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
-import javax.swing.border.EmptyBorder;
 
 class WindowFrame extends JFrame {
 
 	private static final long serialVersionUID = -351798560130670466L;
-	JFrame mainFrame;
-	JTextArea inputMessagePanel;
-	JTextArea outputMessagePanel;
-	JLabel label;
-	JButton toPhp, toJava, toJavaScript, toC, toPython;
-	
+	private final Listener listener;
+	private JFrame mainFrame;
+	private JTextArea inputMessagePanel;
+	private JTextArea outputMessagePanel;
+	private JLabel label;
+	private JButton toPhp, toJava, toJavaScript, toC, toPython;
+	private JPanel buttonPanel;
+	private String inputText;
 	
 	WindowFrame() {
+		listener = new Listener();
 		mainFrame = new JFrame();
 		toPhp = new JButton("PHP");
+		toPhp.addActionListener((e) -> {
+			inputText = inputMessagePanel.getText();
+			listener.scanText(inputText, "PHP");
+		});
 		toJava = new JButton("Java");
+		toJava.addActionListener((e) -> {
+			inputText = inputMessagePanel.getText();
+			listener.scanText(inputText, "Java");
+		});
 		toJavaScript = new JButton("JavaScript");
+		toJavaScript.addActionListener((e) -> {
+			inputText = inputMessagePanel.getText();
+			listener.scanText(inputText, "JavaScript");
+		});
 		toC = new JButton("C");
+		toC.addActionListener((e) -> {
+			inputText = inputMessagePanel.getText();
+			listener.scanText(inputText, "C");
+		});
 		toPython = new JButton("Python");
+		toPython.addActionListener((e) -> {
+			inputText = inputMessagePanel.getText();
+			listener.scanText(inputText, "Python");
+		});
 		label = new JLabel("VRL nyelv feldolgozása");
-		inputMessagePanel = new JTextArea("ide add meg a VRL kódot");
+		inputMessagePanel = new JTextArea("Ide add meg a VRL kódot!");
 		outputMessagePanel = new JTextArea();
 		
-		mainFrame.setBounds(100, 100, 450, 300);
+		buttonPanel = new JPanel();
+		
+		mainFrame.setBounds(100, 100, 720, 480);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setGUI();
 		mainFrame.setVisible(true);
 	}
 	
 	void setGUI() {
-		SpringLayout springLayout = new SpringLayout();
-		mainFrame.getContentPane().setLayout(springLayout);
+		mainFrame.getContentPane().setLayout(new BoxLayout(mainFrame.getContentPane(), BoxLayout.Y_AXIS));
 		mainFrame.getContentPane().setBackground(Color.decode("#DEB877"));
-		
-		springLayout.putConstraint(SpringLayout.WEST, inputMessagePanel, 70, SpringLayout.WEST, mainFrame.getContentPane());
-		springLayout.putConstraint(SpringLayout.NORTH, inputMessagePanel, 30, SpringLayout.NORTH, mainFrame.getContentPane());
-		mainFrame.getContentPane().add(inputMessagePanel);
-		
+				
 		label.setFont(new java.awt.Font("Courier", java.awt.Font.BOLD,20));
 		label.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		mainFrame.getContentPane().add(label);
 		
-		springLayout.putConstraint(SpringLayout.SOUTH, inputMessagePanel, -40, SpringLayout.NORTH, outputMessagePanel);
-		springLayout.putConstraint(SpringLayout.EAST, inputMessagePanel, 0, SpringLayout.EAST, outputMessagePanel);
-		springLayout.putConstraint(SpringLayout.NORTH, outputMessagePanel, 143, SpringLayout.NORTH, mainFrame.getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, outputMessagePanel, 70, SpringLayout.WEST, mainFrame.getContentPane());
-		springLayout.putConstraint(SpringLayout.SOUTH, outputMessagePanel, -10, SpringLayout.SOUTH, mainFrame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, outputMessagePanel, -70, SpringLayout.EAST, mainFrame.getContentPane());
+		mainFrame.getContentPane().add(inputMessagePanel);
+		
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		buttonPanel.add(toPhp);
+		buttonPanel.add(toJava);
+		buttonPanel.add(toJavaScript);
+		buttonPanel.add(toC);
+		buttonPanel.add(toPython);
+
+		mainFrame.getContentPane().add(buttonPanel);
+		
 		outputMessagePanel.setEditable(false);
-		mainFrame.getContentPane().add(outputMessagePanel);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, toPhp, 6, SpringLayout.SOUTH, inputMessagePanel);
-		springLayout.putConstraint(SpringLayout.WEST, toPhp, 70, SpringLayout.WEST, mainFrame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, toPhp, 135, SpringLayout.WEST, mainFrame.getContentPane());
-		mainFrame.getContentPane().add(toPhp);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, toJava, 6, SpringLayout.SOUTH, inputMessagePanel);
-		springLayout.putConstraint(SpringLayout.WEST, toJava, 6, SpringLayout.EAST, toPhp);
-		springLayout.putConstraint(SpringLayout.EAST, toJava, 57, SpringLayout.EAST, toPhp);
-		mainFrame.getContentPane().add(toJava);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, toJavaScript, 6, SpringLayout.SOUTH, inputMessagePanel);
-		springLayout.putConstraint(SpringLayout.WEST, toJavaScript, 10, SpringLayout.EAST, toJava);
-		springLayout.putConstraint(SpringLayout.EAST, toJavaScript, 56, SpringLayout.EAST, toJava);
-		mainFrame.getContentPane().add(toJavaScript);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, toC, 6, SpringLayout.SOUTH, inputMessagePanel);
-		springLayout.putConstraint(SpringLayout.WEST, toC, 7, SpringLayout.EAST, toJavaScript);
-		springLayout.putConstraint(SpringLayout.EAST, toC, 53, SpringLayout.EAST, toJavaScript);
-		mainFrame.getContentPane().add(toC);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, toPython, 6, SpringLayout.SOUTH, inputMessagePanel);
-		springLayout.putConstraint(SpringLayout.WEST, toPython, 11, SpringLayout.EAST, toC);
-		springLayout.putConstraint(SpringLayout.EAST, toPython, 0, SpringLayout.EAST, inputMessagePanel);
-		mainFrame.getContentPane().add(toPython);
-		
+		mainFrame.getContentPane().add(outputMessagePanel);		
 	}
 }
 
--- Crosstranslator/src/main/java/hu/iit/uni_miskolc/horvath23/Crosstranslator/Listener.java
+++ Crosstranslator/src/main/java/hu/iit/uni_miskolc/horvath23/Crosstranslator/Listener.java
@@ -1,61 +1,27 @@
 package hu.iit.uni_miskolc.horvath23.Crosstranslator;
 
 import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-
+import java.io.StringReader;
 import hu.iit.uni_miskolc.horvath23.Crosstranslator.lexer.Scanner;
 import hu.iit.uni_miskolc.horvath23.Crosstranslator.parser.Parser;
-import java_cup.Lexer;
 import java_cup.runtime.ComplexSymbolFactory;
 import java_cup.runtime.ScannerBuffer;
 
 public class Listener extends MouseAdapter {
-
-	JButton button;
-	JLabel fileName;
-	JLabel message;
-	JPanel messagePanel;
-	JScrollPane scrollPane;
-	ArrayList<String> content = new ArrayList<String>();
 	
-	Listener(JButton button,JLabel message, JPanel messagePanel, JScrollPane scrollPane){
-		this.button = button;
-		this.message = message;
-		this.messagePanel = messagePanel;
-		this.scrollPane = scrollPane;
+	Listener(){
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e){
-		JFileChooser chooser = new JFileChooser();
-		chooser.setDialogTitle("Fájl kiválasztása");
-		int result = chooser.showOpenDialog(null);
-		if (result == JFileChooser.APPROVE_OPTION) {
-		    File file = chooser.getSelectedFile();
-		    try {
-		    	//Lexer scanner = new Lexer(new FileReader(file));
-		    	Scanner scanner = new Scanner(new FileReader(file));
-		    	ScannerBuffer buffer = new ScannerBuffer(scanner);
-				Parser parser = new Parser(buffer, new ComplexSymbolFactory());
-				try {
-					parser.parse();
-				} catch (Exception exception) {
-					System.out.println(exception.getMessage());
-				}
-				System.out.println(buffer.getBuffered());
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
+	public void scanText(String text, String goalLanguage) {
+		System.out.println(goalLanguage);
+		Scanner scanner = new Scanner(new StringReader(text));
+    	ScannerBuffer buffer = new ScannerBuffer(scanner);
+		Parser parser = new Parser(buffer, new ComplexSymbolFactory());
+		try {
+			parser.parse();
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
 		}
+		System.out.println(buffer.getBuffered());
 	}
 }
